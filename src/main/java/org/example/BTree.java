850ae76f723e3d5b1f3dad9c50b57906ec9a7146
KacperWS <100286062+KacperWS@users.noreply.github.com>
2024-12-04 17:57:24 +0100

    Code

--- src/main/java/org/example/BTree.java
+++ src/main/java/org/example/BTree.java
@@ -4,20 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BTree {
-    private List<Node> tree;
+    //private List<Node> tree;
+    private Node root;
+    private List<Node> path;
     private List<Long> deletedPages;
     private List<Long> deletedRecords;
     private final int pageSize;
-    private final int treeCapacity; //d value
+    private final int treeCapacity;
+    private Node current = null;    //d value
 
     public BTree(int treeCapacity) {
         this.pageSize = treeCapacity * 4 + 1;
         this.treeCapacity = treeCapacity;
         this.deletedRecords = new ArrayList<>();
         this.deletedPages = new ArrayList<>();
-        this.tree = new ArrayList<>();
+        //this.tree = new ArrayList<>();
+        this.path = new ArrayList<>();
         Node root = new Node(treeCapacity);
-        tree.add(root);
+        this.root = root;
+        current = root;
+        //tree.add(root);
     }
 
     public int getPageSize() {
@@ -28,8 +34,8 @@ public class BTree {
         return treeCapacity;
     }
 
-    public List<Node> getTree() {
-        return tree;
+    public Node getRoot() {
+        return root;
     }
 
     public List<Long> getDeletedPages() {
@@ -37,17 +43,47 @@ public class BTree {
     }
 
     public void insert(int key, long offset) {
-        Node root = tree.getFirst();
-        if(root.getValues().size() ==  2 * treeCapacity){
+        search(key, root);
+        if(current == null)
+            return;
+        if(current.getValues().size() < 2 * treeCapacity){
+            insert2(current);
+        }
+        else if (current == root){
+            Node temp = root;
             Node newRoot = new Node(treeCapacity);
-            tree.set(0, newRoot);
-            newRoot.getValues().add();
+            root = newRoot;
+            newRoot.getChildren().add(temp);
+            split();
+            //insert2(newRoot);
+        }else {
+            if(compensate())
+                return;
+            else
+                split();
+            //insert2(root);
+        }
+
+        /*Node temp = root;
+        if(root.getValues().size() ==  2 * treeCapacity){
+
+            Node newRoot = new Node(treeCapacity);
+            root = newRoot;
+            newRoot.getChildren().add(temp);
+            split();
+            insert2(newRoot);
+            //tree.set(0, newRoot);
+            //newRoot.getValues().add();
             //int place = findPlace(root, key);
             //root.getValues().add(new Element(key, offset));
         }
         else {
+            insert2(root);
+        }*/
+    }
 
-        }
+    private void insert2(Node node) {
+
     }
 
     public int findPlace(Node node, int key){
@@ -60,12 +96,56 @@ public class BTree {
         return 0;
     }
 
-    public void search() {
-
+    public int find(int key, Node node){
+        List<Element> temp = node.getValues();
+        int i = 0;
+        for(i = 0; i < temp.size(); i++){
+            if(temp.get(i).getKey() >= key)
+                return i;
+        }
+        return i;
     }
 
-    public void compensate() {
+    public Element search(int key, Node s) {
+        //Node s = root;
+        path.add(root);
+        if(s.getValues().isEmpty()) {
+            current = root;
+            return null;
+        }
+        else {
+            int x = find(key, s);
+            if(s.getValues().get(x).getKey() == key) {
+                current = null;
+                return s.getValues().get(x);
+            }
+            else {
+                if(s.getPointers().isEmpty()) {
+                    path.add(s);
+                    current = s;
+                }
+                else {
+                    path.add(s);
+                    search(key, s.getChildren().get(x));
+                }
+            }
+        }
+        return null;
+    }
 
+    public boolean compensate() {
+        Node parent = path.get(path.indexOf(current) - 1);
+        List<Node> children = parent.getChildren();
+        if (children.size() < 2)
+            return false; //No sibling to compensate
+
+        int index = children.indexOf(current);
+        Node sibling;
+        if(index < 1){
+            if(children.)
+        }
+        //Node sibling = temp.getChildren().get(temp.getChildren().indexOf(current));
+        return false;
     }
 
     public void split() {
--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class Node {
     private List<Element> values;
     private List<Long> pointers;
+    private List<Node> children;
 
 
     public Node(int d) {
         this.values = new ArrayList<>(2 * d);
         this.pointers = new ArrayList<>(2 * d + 1);
+        this.children = new ArrayList<>(2 * d + 1);
     }
 
     public List<Element> getValues() {
@@ -19,6 +21,10 @@ public class Node {
 
     public List<Long> getPointers() {
         return pointers;
+    }
+
+    public List<Node> getChildren() {
+        return children;
     }
 
     public void setPointers(List<Long> pointers) {
