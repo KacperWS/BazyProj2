1651ddb86247d6ab668b80407550b8212faf82c0
KacperWS <100286062+KacperWS@users.noreply.github.com>
2024-12-05 19:34:52 +0100

    e

--- src/main/java/org/example/DiscIO.java
+++ src/main/java/org/example/DiscIO.java
@@ -1,4 +1,309 @@
 package org.example;
 
-public class DiscIO {
+import java.io.*;
+import java.nio.IntBuffer;
+import java.nio.LongBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.nio.ByteBuffer;
+
+public class DiskIO {
+    private String filename;
+    private String filenameNoext;
+    private int readCounter;
+    private int writeCounter;
+    private BufferedInputStream bis;
+    private BufferedOutputStream bos;
+    private RandomAccessFile raf;
+    private final int recordSize = 7;
+    private final int pageSize;
+    private final int pageSizeBytes;
+    private int recToGenerate = 10000 * recordSize;
+
+    public DiskIO(String filename, int pageSize) throws IOException {
+        this.filename = filename;
+        this.pageSize = pageSize;
+        this.pageSizeBytes = pageSize * 2 * Integer.BYTES + pageSize * 2 * Long.BYTES + (pageSize * 2 + 1) * Long.BYTES;
+        String[] stringArray = filename.split("\\.");
+        this.filenameNoext = stringArray[0];
+    }
+
+    public void setRecToGenerate(int recToGenerate) {
+        this.recToGenerate = recToGenerate * recordSize;
+    }
+
+    public int getRecToGenerate() {
+        return recToGenerate;
+    }
+
+    public String getFilename() {
+        return filenameNoext;
+    }
+
+    public void setFilename(String filename){
+        this.filename = filename;
+        String[] stringArray = filename.split("\\.");
+        this.filenameNoext = stringArray[0];
+    }
+
+    public void closeIN() throws IOException {
+        if (bis != null) {
+            bis.close();
+        }
+    }
+
+    public void closeOUT() throws IOException {
+        if (bos != null) {
+            bos.close();
+        }
+    }
+
+    public void closeRAF() throws IOException {
+        if (raf != null) {
+            raf.close();
+        }
+    }
+
+    public void closeALL() throws IOException {
+        closeIN();
+        closeOUT();
+        closeRAF();
+    }
+
+    public void openIN() throws IOException {
+        this.bis = new BufferedInputStream(new FileInputStream(filename));
+    }
+
+    public void openOUT(String Filename, boolean mode) throws IOException {
+        this.bos = new BufferedOutputStream(new FileOutputStream(Filename, mode));
+    }
+
+    public void openRAF(String op) throws FileNotFoundException {
+        this.raf = new RandomAccessFile(filename, op);
+    }
+
+    public void createDataset() {
+        String fileName = "ter.txt";
+        deleteFile();
+        byte[] binaryData = new byte[4 * this.recToGenerate];
+        Random rand = new Random();
+
+        ByteBuffer test = ByteBuffer.allocate(4 * this.recToGenerate);
+        for (int i = 0; i < binaryData.length / 4; i++) {
+            rand.nextInt();
+            int value = rand.nextInt(8);
+
+            test.putInt(value);
+        }
+        test.flip(); test.get(binaryData);
+
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName))) {
+            int chunkSize = 1024;
+            for (int i = 0; i < binaryData.length; i += chunkSize) {
+                int bytesToWrite = Math.min(chunkSize, binaryData.length - i);
+                bos.write(binaryData, i, bytesToWrite);
+            }
+
+            System.out.println("Binary data saved successfully in chunks to " + (4 * this.recToGenerate));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Record> read(int bufferSize, long offset) throws IOException{
+
+        byte[] buffer = new byte[pageSize * Integer.BYTES * recordSize];
+        List<Record> lista = new ArrayList<>();
+        if (bis.read(buffer) != -1) {
+            ByteBuffer bufferme;
+            bufferme = ByteBuffer.wrap(buffer);
+            IntBuffer test = bufferme.asIntBuffer();
+            int i = 0; int hmm = this.recordSize;
+            int[] array = new int [hmm];
+            while(i < test.capacity()){
+                array[i%hmm] = test.get(i);
+                i++;
+                if(i%hmm == 0){
+                    Record newRecord = new Record(Arrays.copyOf(array, array.length));
+                    /*if (newRecord.isEmpty())
+                        break;*/
+                    lista.add(newRecord);
+                }
+            }
+        }else {
+            closeIN(); //File ended
+            return null; //Indicates that file ended
+        }
+        readCounter++;
+        return lista;
+    }
+
+    public Node read(int pageNumber) throws IOException{
+        openRAF("r");
+        byte[] buffer = new byte[pageSizeBytes]; //2d keys + 2d keys offsets and 2d+1 offsets
+        //List<Record> lista = new ArrayList<>();
+        Node temp = new Node(pageSize);
+        raf.seek((long) pageNumber * pageSizeBytes);
+        if (bis.read(buffer) != -1) {
+            ByteBuffer bufferme;
+            bufferme = ByteBuffer.wrap(buffer);
+            IntBuffer buffer1 = bufferme.asIntBuffer();
+            LongBuffer buffer2 = bufferme.asLongBuffer();
+            for (int i = 0; i < pageSize * 2; i++)
+                temp.getValues().add(new Element(buffer1.get(i), buffer2.get(i + pageSize)));
+            for (int i = 0, j = pageSize * 3; i < pageSize * 2 + 1; i++ , j++)
+                temp.getPointers().add(buffer2.get(j));
+        }else {
+            closeRAF(); //File ended
+            return null; //Indicates that file ended
+        }
+        readCounter++;
+        closeRAF();
+        return temp;
+    }
+
+    public void save(Node node, int pageNumber) throws IOException{
+        //openOUT(addon, true);
+        openRAF("w");
+        raf.seek((long) pageNumber * pageSizeBytes);
+        byte[] binaryData = new byte[pageSizeBytes];
+        ByteBuffer temp = ByteBuffer.allocate(pageSizeBytes);
+        //Puts elements to buffer /key /offset
+        for(int i = 0; i < pageSize * 2; i++)
+            temp.putInt(node.getValues().get(i).getKey());
+        for(int i = 0; i < pageSize * 2; i++)
+            temp.putLong(node.getValues().get(i).getOffset());
+
+        for (int i = 0; i < pageSize * 2 + 1; i++) //Pointers to childs
+            temp.putLong(node.getPointers().get(i));
+
+        temp.flip();
+
+        closeRAF();
+    }
+
+    public boolean sortHere(int n, Buffer buffer, int bufferSize) throws IOException {
+        openRAF("r");
+        byte[] bufferByte = new byte[bufferSize];
+        List<Record> lista = new ArrayList<>();
+        raf.seek(buffer.getJump() + buffer.getBytesRead());
+        if (raf.read(bufferByte) != -1) {
+            ByteBuffer bufferme;
+            bufferme = ByteBuffer.wrap(bufferByte);
+            IntBuffer test = bufferme.asIntBuffer();
+            int i = 0; int hmm = this.recordSize;
+            int[] array = new int [hmm];
+            while(i < test.capacity()){
+                array[i%hmm] = test.get(i);
+                i++;
+                if(i%hmm == 0){
+                    Record newRecord = new Record(Arrays.copyOf(array, array.length));
+                    /*if (newRecord.isEmpty())
+                        break;*/
+                    lista.add(newRecord);
+                }
+            }
+        }else {
+            closeRAF(); //File ended
+            return true; //Indicates that file ended
+        }
+        //buffer.updateBytesRead(lista.size() * 6 * Integer.BYTES);
+        //buffer.setBuffer(lista); readCounter++;
+        closeRAF();
+        return false;
+    }
+
+    public void deleteFile(){
+        Path path = Paths.get(filename);
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            System.out.println("Failed to delete the file. Error: " + e.getMessage());
+        }
+    }
+
+    public void showFile(){
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            byte[] byteBuffer = new byte[4];
+            int i = 0;
+            int[] temp = new int[6];
+            while (fis.read(byteBuffer) != -1) {
+                ByteBuffer byteBufferWrapper = ByteBuffer.wrap(byteBuffer);
+                int number = byteBufferWrapper.getInt();
+                if(i%6 == 0 && i > 0){
+                    int x = temp[5];
+                    int suma = 0, xpower = x;
+                    suma += temp[0] + temp[1] * xpower;
+                    suma+= temp[2] * (xpower *= x);
+                    suma+= temp[3] * (xpower *= x);
+                    suma+= temp[4] * (xpower * x);
+                    System.out.println("Rekord = " + suma);
+                }
+                temp[i%6] = number;
+                if(i%6 == 0)
+                    System.out.print(i/6 + ". ");
+                System.out.print(number + " ");
+                i++;
+            }
+            int x = temp[5];
+            int suma = 0, xpower = x;
+            suma += temp[0] + temp[1] * xpower;
+            suma+= temp[2] * (xpower *= x);
+            suma+= temp[3] * (xpower *= x);
+            suma+= temp[4] * (xpower * x);
+            System.out.println("Rekord = " + suma);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showResults(){
+        System.out.println("Reads: " + readCounter + " Writes: " + writeCounter);
+    }
+
+    public void check(){
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            byte[] byteBuffer = new byte[4];
+            int i = 0;
+            int[] temp = new int[6];
+            int suma = 0; int next = -1; int check = 0;
+            while (fis.read(byteBuffer) != -1) {
+                ByteBuffer byteBufferWrapper = ByteBuffer.wrap(byteBuffer);
+                int number = byteBufferWrapper.getInt();
+                if(i%6 == 0 && i > 0){
+                    int x = temp[5];
+                    suma = 0; int xpower = x;
+                    suma += temp[0] + temp[1] * xpower;
+                    suma+= temp[2] * (xpower *= x);
+                    suma+= temp[3] * (xpower *= x);
+                    suma+= temp[4] * (xpower * x);
+                    if(next <= suma)
+                        check++;
+                    else check--;
+                    //System.out.println("Rekord = " + suma + " next " + next);
+                    next = suma;
+                }
+                temp[i%6] = number;
+                //System.out.print(number + " ");
+                i++;
+            }
+            int x = temp[5];
+            suma = 0;int xpower = x;
+            suma += temp[0] + temp[1] * xpower;
+            suma+= temp[2] * (xpower *= x);
+            suma+= temp[3] * (xpower *= x);
+            suma+= temp[4] * (xpower * x);
+            if(next <= suma)
+                check++;
+            else
+                check--;
+            //next = suma;
+            System.out.println("Rekord = " + check);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main() {
         BTree temp = new BTree(1);
         for (int i = 0; i < 10; i++)
-            temp.insert(i,1);
+            temp.insert(i,i);
         Element t = temp.search(2, temp.getRoot());
         temp.display();
         //System.out.print(Math.round(5/2.0));
